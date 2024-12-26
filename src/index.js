91357fe4f68c842ffa9adfd33837468b4a554887
Yadav-creators <yadav.creators@gmail.com>
2024-12-26 16:48:27 +0530

    Test3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ParallaxProvider } from "react-scroll-parallax";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./styles/bootstrap.min.css";
 import "./styles/animate.min.css";
 import "./styles/fontawesome-all.min.css";
@@ -19,7 +18,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ParallaxProvider>
-      <SpeedInsights />
       <App />
     </ParallaxProvider>
   </BrowserRouter>
